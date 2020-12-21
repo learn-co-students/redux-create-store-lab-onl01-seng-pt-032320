f5d116763676a81a6281f0656c4f003c6d16b65f
EvanTheSmith <ProfessionalEmail@Zoho.com>
2020-12-21 09:29:21 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
-// write your createStore function here
+function createStore(reducer) {
+  let state;
+  function dispatch(action) { state = reducer(state, action); render(); } 
+  function getState() { return state; };
+  return { dispatch, getState };
+}
+
+let store = createStore(candyReducer);
 
 function candyReducer(state = [], action) {
   switch (action.type) {
@@ -20,3 +27,4 @@ function render() {
 
 // Use your createStore function and the functions provided here to create a store.
 // Once the store is created, call an initial dispatch.
+store.dispatch({ type: 'baked potatoes' }); 
